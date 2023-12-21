930c3b4b723f0754c300bdc57b9ed973cb444a31
Konrad Wienecke <konrad.wienecke@gmail.com>
2023-12-21 17:50:57 +0100

    Different way of defining todo/ path with one urls.py

--- matchmaker/urls.py
+++ matchmaker/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from todo_list import views
 
 urlpatterns = [
-    path("todo/", include("todo_list.urls")),
+    path("todo/", views.index, name="index"),
     path("admin/", admin.site.urls),
 ]
