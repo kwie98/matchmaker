47c52dcfabecbfe933a12fbcef1303c7ce6deb94
Konrad Wienecke <konrad.wienecke@gmail.com>
2023-12-21 18:01:25 +0100

    Revert "Different way of defining todo/ path with one urls.py"
    This reverts commit 930c3b4b723f0754c300bdc57b9ed973cb444a31.

--- matchmaker/urls.py
+++ matchmaker/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from todo_list import views
+from django.urls import include, path
 
 urlpatterns = [
-    path("todo/", views.index, name="index"),
+    path("todo/", include("todo_list.urls")),
     path("admin/", admin.site.urls),
 ]
