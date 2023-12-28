db84d5ada2e07b541b022ece099c4d2b6798b087
Konrad Wienecke <konrad.wienecke@gmail.com>
2023-12-28 13:56:26 +0100

    Add __str__ for Item

--- todo_list/models.py
+++ todo_list/models.py
@@ -11,3 +11,7 @@ class Item(models.Model):
 
     text = models.TextField()
     status = models.CharField(max_length=1, choices=Status)
+
+
+    def __str__(self):
+        return f"[{self.status}] {self.text}"
