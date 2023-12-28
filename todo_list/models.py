62be805a3106d2142572559cf81ea30f73cf9530
Konrad Wienecke <konrad.wienecke@gmail.com>
2023-12-28 15:11:06 +0100

    Trying some migrating

--- todo_list/models.py
+++ todo_list/models.py
@@ -4,14 +4,15 @@ from django.utils.translation import gettext_lazy
 
 # Create your models here.
 class Item(models.Model):
-    class Status(models.TextChoices):
-        PENDING = "P", gettext_lazy("Pending")
-        DONE = "D", gettext_lazy("Done")
-        CANCELLED = "C", gettext_lazy("Cancelled")
-
     text = models.TextField()
-    status = models.CharField(max_length=1, choices=Status)
-
+    status = models.CharField(
+        max_length=1,
+        choices=(
+            ("P", gettext_lazy("Pending")),
+            ("D", gettext_lazy("Done")),
+            ("C", gettext_lazy("Cancelled")),
+        ),
+    )
 
     def __str__(self):
         return f"[{self.status}] {self.text}"
