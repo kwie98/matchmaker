21cd7681054423645397ca9b39ff33e3f85faed3
Konrad Wienecke <konrad.wienecke@gmail.com>
2023-12-28 15:11:37 +0100

    Add views for all items and single item

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("items/", views.items, name="items"),
+    path("item/<int:item_id>/", views.item, name="items"),
+]
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,6 +1,21 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+
+from todo_list.models import Item
+
 
 # Create your views here.
 def index(request):
     return HttpResponse(b"Welcome to the todo list :)")
+
+
+def items(request):
+    items = Item.objects.all()
+    return HttpResponse(items)
+
+
+def item(request, item_id: int):
+    try:
+        item = Item.objects.get(id=item_id)
+        return HttpResponse(f"{item}")
+    except Exception as e:
+        return HttpResponse(e)
