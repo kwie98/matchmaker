713db3c5cd75e1551cc1ec76ed06bac253a9898c
Konrad Wienecke <konrad.wienecke@gmail.com>
2023-12-28 12:02:02 +0100

    Correct Item model and migration

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import gettext_lazy
+
 
 # Create your models here.
+class Item(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "P", gettext_lazy("Pending")
+        DONE = "D", gettext_lazy("Done")
+        CANCELLED = "C", gettext_lazy("Cancelled")
+
+    text = models.TextField()
+    status = models.CharField(max_length=1, choices=Status)
