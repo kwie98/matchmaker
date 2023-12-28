95fe31a3dd12b97c207994466ef57d8cc789b6f1
Konrad Wienecke <konrad.wienecke@gmail.com>
2023-12-28 15:19:07 +0100

    Add typings for views

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,19 +1,19 @@
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 
 from todo_list.models import Item
 
 
 # Create your views here.
-def index(request):
-    return HttpResponse(b"Welcome to the todo list :)")
+def index(_: HttpRequest) -> HttpResponse:
+    return HttpResponse("Welcome to the todo list :)")
 
 
-def items(request):
+def items(_: HttpRequest) -> HttpResponse:
     items = Item.objects.all()
     return HttpResponse(items)
 
 
-def item(request, item_id: int):
+def item(_: HttpRequest, item_id: int) -> HttpResponse:
     try:
         item = Item.objects.get(id=item_id)
         return HttpResponse(f"{item}")
