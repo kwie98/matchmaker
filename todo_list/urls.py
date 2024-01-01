68dd701637c9b9bf24638494acd4117aab671f4d
Konrad Wienecke <konrad.wienecke@gmail.com>
2024-01-01 23:58:37 +0100

    Use `url` in template

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "todo_list"
 urlpatterns = [
     path("", views.index, name="index"),
     path("items/", views.items, name="items"),
-    path("item/<int:item_id>/", views.item, name="items"),
+    path("item/<int:item_id>/", views.item, name="item"),
 ]
