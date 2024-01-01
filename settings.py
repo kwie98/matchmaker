aec66fd86aeb01c0176615b445030c584749a2fb
Konrad Wienecke <konrad.wienecke@gmail.com>
2024-01-02 00:17:14 +0100

    Switch to flat project structure

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "matchmaker.urls"
+ROOT_URLCONF = "urls"
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "matchmaker.wsgi.application"
+WSGI_APPLICATION = "wsgi.application"
 
 
 # Database
