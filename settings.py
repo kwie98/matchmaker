fe2281fcce54b01f6bef936cccf0c9a64ecfc940
Konrad Wienecke <konrad.wienecke@gmail.com>
2024-01-26 11:12:07 +0100

    Set DEBUG to False in deploy

--- settings.py
+++ settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent
 SECRET_KEY = os.environ.get("SECRET_KEY")  # not available during docker image build time
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost"]
 
